f196811ccf6ed9b16a24c7bc351ac990e1c9025a
Jason Real <tinidian@gmail.com>
2024-09-13 12:58:48 -0600

    Corrected typo in error message of code

--- Assignment_Part_1.py
+++ Assignment_Part_1.py
@@ -3,14 +3,14 @@ while True:
       num1 = int(input('Enter Number 1: '))
       break
     except ValueError:
-       print('Oops! That was no a valid number.  Try again...')
+       print('Oops! That was not a valid number.  Try again...')
 
 while True:
     try:
       num2 = int(input('Enter Number 2: '))
       break
     except ValueError:
-       print('Oops! That was no a valid number.  Try again...')
+       print('Oops! That was not a valid number.  Try again...')
 
 print('Sum: ', num1 + num2)
 print('Difference: ', num1 - num2)
