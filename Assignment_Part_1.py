f2039e8b7bcba133dc2ae5c90670c9b29499aefa
Jason Real <tinidian@gmail.com>
2024-09-13 10:20:47 -0600

    Adjusted white space to make output more readable

--- Assignment_Part_1.py
+++ Assignment_Part_1.py
@@ -1,16 +1,16 @@
 while True:
     try:
-      num1 = int(input('Enter Number 1:'))
+      num1 = int(input('Enter Number 1: '))
       break
     except ValueError:
        print('Oops! That was no a valid number.  Try again...')
 
 while True:
     try:
-      num2 = int(input('Enter Number 2:'))
+      num2 = int(input('Enter Number 2: '))
       break
     except ValueError:
        print('Oops! That was no a valid number.  Try again...')
 
-print('Sum:', num1 + num2)
-print('Difference:', num1 - num2)
+print('Sum: ', num1 + num2)
+print('Difference: ', num1 - num2)
--- Assignment_Part_2.py
+++ Assignment_Part_2.py
@@ -1,20 +1,20 @@
 while True:
     try:
-      num1 = int(input('Enter Number 1:'))
+      num1 = int(input('Enter Number 1: '))
       break
     except ValueError:
        print('Oops! That was no a valid number.  Try again...')
 
 while True:
     try:
-      num2 = int(input('Enter Number 2:'))
+      num2 = int(input('Enter Number 2: '))
       break
     except ValueError:
        print('Oops! That was no a valid number.  Try again...')
 
-print('Product:', num1 * num2)
+print('Product: ', num1 * num2)
 
 try:
-  print('Quotient:', num1 / num2)
+  print('Quotient: ', num1 / num2)
 except ZeroDivisionError:
    print('Oops!  You tried to divide by Zero.')
